80bc55509c6e8e263d436bd26f17e8a423beaefe
Michael Toscano <33842918+toscano@users.noreply.github.com>
2024-03-14 17:58:04 -0400

    Metadata working

--- custom_components/autonomic/controller.py
+++ custom_components/autonomic/controller.py
@@ -19,7 +19,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.util.dt as dt_util
 
-from .const import DOMAIN, MIN_VERSION_REQUIRED, MODE_UNKNOWN,  MODE_STANDALONE, MODE_MRAD, RETRY_CONNECT_SECONDS, PING_INTERVAL
+from .const import DOMAIN, MIN_VERSION_REQUIRED, MODE_UNKNOWN,  MODE_STANDALONE, MODE_MRAD, RETRY_CONNECT_SECONDS, PING_INTERVAL, TICK_THRESHOLD_SECONDS, TICK_UPDATE_SECONDS
 
 LOGGER = logging.getLogger(__package__)
 
@@ -41,7 +41,9 @@ class Controller:
         self._version: str = ""
 
         self._zoneEntities = []
+        self._zoneEntitiesByGuid = {}
         self.is_connected = False
+        self._events = {}
 
 
     async def async_check_connection(self) -> bool:
@@ -74,7 +76,7 @@ class Controller:
         self._version = data['root']['device']['modelNumber']
         LOGGER.debug(f"Version: {self._version}")
 
-        version = self.version
+        version = self._version
         idx = version.find('Debug')
         if idx < 0:
             idx = version.find(' ')
@@ -136,6 +138,9 @@ class Controller:
         workToDo = True
         while workToDo:
             try:
+                if self._closing:
+                    return
+
                 LOGGER.info(f"Connecting to {self._host}:{self._port}")
 
                 reader, writer = await asyncio.open_connection(self._host, self._port)
@@ -168,18 +173,18 @@ class Controller:
             self.send('subscribeeventsall')
             self.send('getstatus')
 
-            self.send('mrad.browsezones')
+            self.send('mrad.browseallzones')
             self.send('mrad.browsezonegroups')
 
 
-        self._ioloop_future = asyncio.ensure_future(self._ioloop(reader, writer))
+        self.async_io_loop_future = asyncio.ensure_future(self.async_io_loop(reader, writer))
 
         LOGGER.info(f"Connected to {self._host}:{self._port}")
         self.is_connected = True
 
         return
 
-    async def _ioloop(self, reader, writer):
+    async def async_io_loop(self, reader, writer):
 
         self._queue_future = asyncio.ensure_future(self._cmd_queue.get())
 
@@ -249,7 +254,7 @@ class Controller:
 
     async def async_check_ping(self, now=None):
         """Maybe send a ping."""
-        if (self.is_connected == False):
+        if (self.is_connected == False or self._closing):
             return
 
         if (self._last_inbound_data_utc + PING_INTERVAL + PING_INTERVAL < dt_util.utcnow() ):
@@ -280,21 +285,281 @@ class Controller:
         LOGGER.debug(f"-->{cmd}")
         self._cmd_queue.put_nowait(cmd)
 
+    def get_event(self, entityId, eventName):
+
+        key = f'{entityId}.{eventName}'
+        if key not in self._events:
+            return None
+        else:
+            return self._events[key]
+
     def _process_response(self, res):
         try:
 
             s = str(res, 'utf-8').strip()
-            LOGGER.debug(f"<--{s}")
+            # _LOGGER.debug("%s:<--%s", self.host, s)
+
+            if s.startswith('<Zones'):
+                self._process_mrad_zone_response(s)
+            elif s.startswith('<ZoneGroups'):
+                self._process_mrad_zone_group_response(s)
+            elif s.startswith('MRAD.'):
+                self._process_mrad_event(s)
+            #elif s.startswith('<Instances'):
+            #    self._process_standalone_instance_response(s)
+            elif s.startswith('ReportState') or s.startswith('StateChanged'):
+                self._process_instance_event(s)
+            #else:
+            #    _LOGGER.info("%s:unprocessed<--%s", self.host, s)
+
+            return s
 
         except Exception as e:
             LOGGER.exception(f"_process_response ex {e}")
             # some error occurred, re-connect may fix that
             self.send('quit')
 
-    @property
-    def name(self) -> str:
-        return self._name
+    def _process_mrad_zone_response(self, res):
+        """Response to BrowseAllZones"""
+        data = xmltodict.parse(res, force_list=('Zone',))
 
-    @property
-    def version(self) -> str:
-        return self._version
+        #  There's a chance that the Zone count is zero while the MMS is starting up... That's handled as an exception/reconnect
+        for zone in data['Zones']['Zone']:
+            # <Zones total="5" start="1" more="false" art="false" alpha="false" displayAs="List">
+            #    <Zone guid="00000001-5ace-e5da-ba88-8cf58dd178f2"
+            #          name="Office"
+            #          dna="name"
+            #          id="Zone_1"
+            #          isOn="True"
+            #          sourceId="20000"
+            #          sourceName="Player A"
+            #          gId="00000000-0000-4e20-0000-000000000000"
+            #          gName="ZG_1"
+            #          gPwr="1"
+            #          gVol="0"
+            #          gSrc="1"
+            #          sId="20000"
+            #          sGuid="11a7df11-bbb4-0586-4df2-b184f9ded057"
+            #          m1="Pandora: Talking Heads Radio"
+            #          m2="The Rolling Stones"
+            #          m3="Hot Rocks (1964-1971) (Remastered)"
+            #          m4="Honky Tonk Women"
+            #          mArt=""
+            #          iconId="Source" />
+            guid    = zone['@guid']
+            sourceId= f"Source_{zone['@sourceId']}"
+            name    = zone['@name']
+            id      = zone['@id']
+
+            if guid in self._zoneEntitiesByGuid:
+                found = self._zoneEntitiesByGuid[guid]
+                found.set_name_source_and_group( newName = name, newSourceId = sourceId )
+            else:
+                # mrad zone
+                found = None
+                for zone in self._zoneEntities:
+                    if zone._mms_zone_id == id:
+                        LOGGER.info(f"DISCOVERED MRAD ZONE: {id} {name}")
+                        found = zone
+                        break
+                if found is not None:
+                    self._zoneEntitiesByGuid[guid] = found
+                    found.set_name_source_and_group( newName = name, newSourceId = sourceId )
+
+    def _process_mrad_zone_group_response(self, res):
+        # This is a kludge that allows us to process <vol> and <src> zones as one element
+        res = res.replace("</vol>", "")
+        res = res.replace("<src>", "")
+        res = res.replace("</src>", "</vol>")
+
+        data = xmltodict.parse(res, force_list=('ZoneGroup',))
+
+        for group in data['ZoneGroups']['ZoneGroup']:
+            #<ZoneGroups total="3" start="1" more="false" art="false" alpha="false" displayAs="List" utcNow="2018-03-09T16:12:22Z" srceAvail="1" srceId="262c9674-9cb2-8860-e31a-0deefbddc26a" srceMmsAddr="192.168.1.80:5004" srceMmsInst="Player_B@0050C2FD2BF2">
+            # <ZoneGroup guid="00000000-0000-4e20-0000-000000000000" name="ZG_1" dna="name" isSearchable="false" button="0" sId="20000" sGuid="11a7df11-bbb4-0586-4df2-b184f9ded057" m1="Pandora: Beck Radio" m2="Cake" m3="B-Sides And Rarities" m4="War Pigs" mArt="http://192.168.1.80:5005/GetArt?instance=Player_A@0050C2FD2BF2&amp;guid=ab4bad9c-6f12-4a61-7466-85832dbc940c&amp;ticks=636561900103465640" iconId="Source">
+            #     <vol>
+            #         <zone eventId="Zone_1" guid="00000001-5ace-e5da-ba88-8cf58dd178f2" name="MT Office" dna="name" icon="Zone" on="1" volume="32" mute="0" />
+            #         <zone eventId="Zone_2" guid="00000002-5ace-e5da-ba88-8cf58dd178f2" name="MT Headphones" dna="name" icon="Zone" on="1" volume="30" mute="1" />
+            #         <zone eventId="Zone_5" guid="00000005-85df-222c-1bf3-696cf573cf56" name="MT Rack I" dna="name" icon="Zone" on="1" volume="28" mute="0" />
+            #         <zone eventId="Zone_6" guid="00000006-85df-222c-1bf3-696cf573cf56" name="MT Rack II" dna="name" icon="Zone" on="1" volume="30" mute="1" />
+            #         <zone eventId="Zone_7" guid="00000007-85df-222c-1bf3-696cf573cf56" name="MT Rack III" dna="name" icon="Zone" on="1" volume="30" mute="1" />
+            #         <zone eventId="Zone_8" guid="00000008-85df-222c-1bf3-696cf573cf56" name="MT Rack IV" dna="name" icon="Zone" on="1" volume="30" mute="1" />
+            #     </vol>
+            #     <src>
+            #         <zone eventId="Zone_1" guid="00000001-5ace-e5da-ba88-8cf58dd178f2" name="MT Office" dna="name" icon="Zone" on="1" />
+            #         <zone eventId="Zone_5" guid="00000005-85df-222c-1bf3-696cf573cf56" name="MT Rack I" dna="name" icon="Zone" on="1" />
+            #     </src>
+            #     <Sources>
+            #         <Source guid="11a7df11-bbb4-0586-4df2-b184f9ded057" name="Player A" dna="name" isSearchable="false" fqn="Player_A@0050C2FD2BF2" smart="1" next="1" sId="20000" iconId="Source" />
+            #         <Source guid="262c9674-9cb2-8860-e31a-0deefbddc26a" name="Player B" dna="name" isSearchable="false" fqn="Player_B@0050C2FD2BF2" smart="1" next="0" sId="20001" iconId="Source" />
+            #         <Source guid="000027f5-5ace-e5da-ba88-8cf58dd178f2" name="CD120-1" dna="name" isSearchable="false" fqn="" smart="0" next="0" sId="10101" iconId="Source" />
+            #     </Sources>
+            # </ZoneGroup>
+            groupGuid= group.get('@guid', "")
+            groupName= group.get('@name', "")
+            sId      = group.get('@sId', "0")
+            sourceId = f"Source_{sId}"
+            mArt     = group.get('@mArt', "" )
+
+            if mArt == "":
+                self._events[f'{sourceId}.mArt'         ]=None
+                self._events[f'{sourceId}.MetaData1'    ]=None
+                self._events[f'{sourceId}.MetaData2'    ]=None
+                self._events[f'{sourceId}.MetaData3'    ]=None
+                self._events[f'{sourceId}.MetaData4'    ]=None
+                self._events[f'{sourceId}.TrackDuration']=None
+                self._events[f'{sourceId}.TrackTime'    ]=None
+                self._events[f'{sourceId}.TrackTimeUtc' ]=None
+                self._events[f'{sourceId}.Shuffle'      ]=None
+                self._events[f'{sourceId}.SmartSource'  ]=False
+                self._events[f'{sourceId}.MediaControl' ]='Unknown'
+            else:
+                self._events[f'{sourceId}.mArt'         ]=mArt
+                self._events[f'{sourceId}.SmartSource'  ]=True
+
+            sources = []
+
+            # Make sure we've got a list to process which isn't true if there
+            # is only one source enabled on the MMS. Crazy right?
+            if isinstance(group['Sources']['Source'], list):
+                sourcesList = group['Sources']['Source']
+            else:
+                sourcesList = [group['Sources']['Source']]
+
+            for source in sourcesList:
+                fqn = source.get('@name', "")
+                if fqn == "":
+                    fqn = source['@fqn'].split("@")[0]
+
+                # Add that to the list of ALL sources for this (these) zone(s)
+                sources.append(fqn)
+
+                # And make sure that's correct in the event table
+                sid = source.get('@sId', "")
+                key = f'Source_{sid}.QualifiedSourceName'
+                self._events[key] = fqn
+
+            # Now set the available sources into the zone (zones)
+            for vZone in group['vol']['zone']:
+                name = vZone['@name']
+                eventId = vZone['@eventId']
+                key = f'{eventId}.SourceList'
+                self._events[key]=sources
+
+                # Ensure that the sourceId is set correctly for the zone
+                guid = vZone["@guid"]
+                if guid in self._zoneEntitiesByGuid:
+                    found = self._zoneEntitiesByGuid[guid]
+                    found.set_name_source_and_group( newName = name, newSourceId = sourceId, newGroupGuid = groupGuid, newGroupName = groupName )
+                else:
+                    found = None
+                    for zone in self._zoneEntities:
+                        if zone._mms_zone_id == eventId:
+                            LOGGER.info(f"DISCOVERED MRAD ZONE: {eventId} {name}")
+                            found = zone
+                            break
+                    if found is not None:
+                        self._zoneEntitiesByGuid[guid] = found
+                        found.set_name_source_and_group( newName = name, newSourceId = sourceId, newGroupGuid = groupGuid, newGroupName = groupName )
+
+    def _process_mrad_event(self, res):
+        # Parse...
+        # MRAD.ReportState Zone_1 ZoneGain=0
+        splits = res.split(' ')
+        nv = splits[2].split('=')
+        eventName = nv[0]
+        pEq = res.find('=')
+        entityId = splits[1]
+
+        key = f'{entityId}.{eventName}'
+        eventValue = res[pEq+1:]
+
+        # Update our object for the first few TrackTime events
+        # then only once every TICK_UPDATE_SECONDS
+        if eventName == 'TrackTime':
+            eventValue = eventValue.replace("00:00:00", "0")
+            if key in self._events:
+                if int(eventValue) > TICK_THRESHOLD_SECONDS:
+                    if int(eventValue) % TICK_UPDATE_SECONDS != 0:
+                        return
+
+        self._events[key]=eventValue
+
+        # Manufacture TrackTimeUtc and since TrackTime
+        # only occurs for SmartSources manufacture that too...
+        if eventName == 'TrackTime':
+            eventName = 'TrackTimeUtc'
+            key = f'{entityId}.{eventName}'
+            eventValue = dt_util.utcnow()
+            self._events[key]=eventValue
+
+            eventName = 'SmartSource'
+            key = f'{entityId}.{eventName}'
+            eventValue = True
+            self._events[key]=eventValue
+
+        # Schedule an update for the associated Zone(s)
+        for zone in self._zoneEntities:
+            if zone._mms_zone_id == entityId:
+                zone.schedule_update_ha_state()
+            elif zone._mms_source_id == entityId:
+                zone.schedule_update_ha_state()
+
+    def _process_instance_event(self, res):
+        # Parse...
+        # StateChanged Player_A TrackTime=263
+        splits = res.split(' ')
+        nv = splits[2].split('=')
+        eventName = nv[0]
+        pEq = res.find('=')
+        entityId = splits[1]
+
+        key = f'{entityId}.{eventName}'
+        eventValue = res[pEq+1:]
+
+        # Update our object for the first few TrackTime events
+        # then only once every TICK_UPDATE_SECONDS
+        if eventName == 'TrackTime':
+            eventValue = eventValue.replace("00:00:00", "0")
+            if key in self._events:
+                if int(eventValue) > TICK_THRESHOLD_SECONDS:
+                    if int(eventValue) % TICK_UPDATE_SECONDS != 0:
+                        return
+
+        self._events[key]=eventValue
+
+        # Manufacture TrackTimeUtc and since TrackTime
+        # only occurs for SmartSources manufacture that too...
+        if eventName == 'TrackTime':
+            eventName = 'TrackTimeUtc'
+            key = f'{entityId}.{eventName}'
+            eventValue = dt_util.utcnow()
+            self._events[key]=eventValue
+
+            eventName = 'SmartSource'
+            key = f'{entityId}.{eventName}'
+            eventValue = True
+            self._events[key]=eventValue
+
+        if self._mode == MODE_STANDALONE:
+            # Shortcut to better art
+            if eventName == 'MediaArtChanged':
+                self.send('browseinstances')
+                return
+
+            # Schedule an update for the associated Zone(s)
+            for zone in self._zoneEntities:
+                if zone._mms_zone_id == entityId:
+                    zone.schedule_update_ha_state()
+                elif zone._mms_source_id == entityId:
+                    zone.schedule_update_ha_state()
+
+        else:
+            for zone in self._zoneEntities:
+                key = f'{zone._mms_source_id}.QualifiedSourceName'
+                if key in self._events:
+                    val = self._events[key]
+                    if val is not None:
+                        if val == entityId:
+                            zone.schedule_update_ha_state()
+
--- custom_components/autonomic/media_player.py
+++ custom_components/autonomic/media_player.py
@@ -16,6 +16,8 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity_registry import RegistryEntryHider
+import homeassistant.helpers.entity_registry as er
 
 from . import controller
 from .const import DOMAIN, MANUFACTURER, MODE_MRAD, MODE_STANDALONE
@@ -53,6 +55,12 @@ class MmsZone(MediaPlayerEntity):
         self._attr_device_class = MediaPlayerDeviceClass.SPEAKER
 
         # Member variables that will change as things go...
+
+        # from MMS
+        self._mms_groupGuid = ""
+        self._mms_groupName = ""
+
+        # from HASS
         self._attr_app_name = ""
         self._extra_attributes = {}
         self._isOn = False
@@ -94,10 +102,14 @@ class MmsZone(MediaPlayerEntity):
         """
 
         if controller._mode == MODE_MRAD:
+            self._mms_zone_id = f"Zone_{int(indexOrName)}"
+            self._mms_source_id = ""
             self._name = f"{controller._name} Zone {int(indexOrName):02d}"
             self._attr_unique_id = f"{entry.unique_id}_zone_{int(indexOrName):02d}"
 
         elif controller._mode == MODE_STANDALONE:
+            self._mms_zone_id = None
+            self._mms_source_id = f"{indexOrName}".replace(' ', '_')
             self._name = f"{controller._name} {indexOrName}"
             self._attr_unique_id = f"{entry.unique_id}_{indexOrName}"
 
@@ -117,6 +129,34 @@ class MmsZone(MediaPlayerEntity):
         except Exception as error:  # pylint: disable=broad-except
             LOGGER.debug("State update failed.")
 
+    def set_name_source_and_group(self, newName: str | None = None, newSourceId: str | None = None, newGroupGuid: str | None = None, newGroupName: str | None = None):
+
+        isDirty = False
+
+        if newName is not None and newName != self._name:
+            if self._mms_zone_id is not None and self._mms_zone_id==newName.replace(' ', '_'):
+                LOGGER.debug(f"Attempt to hide {newName}")
+                entity_registry = er.async_get(self._hass)
+                self.registry_entry = entity_registry.async_update_entity(self.entity_id, hidden_by = RegistryEntryHider.INTEGRATION )
+
+            self._name = newName
+            isDirty = True
+
+
+        if newSourceId is not None and newSourceId != self._mms_source_id:
+            self._mms_source_id = newSourceId
+            isDirty = True
+
+        if newGroupGuid is not None:
+            self._mms_groupGuid = newGroupGuid
+
+        if newGroupName is not None:
+            self._mms_groupName = newGroupName
+
+        if isDirty:
+            self.schedule_update_ha_state()
+
+
     @property
     def name(self):
         """Return the name of the entity."""
@@ -133,13 +173,74 @@ class MmsZone(MediaPlayerEntity):
         """
         return False
 
+    def GetSourceEvent(self, event_id : str ) -> str | None:
+
+        if self._controller.is_connected:
+            sourceId = self._mms_source_id
+            if sourceId is None:
+                return None
+
+            val = self._controller.get_event(sourceId, event_id)
+
+            if val is not None:
+                return val
+
+            if self._controller._mode == MODE_MRAD:
+                val = self._controller.get_event(sourceId, 'QualifiedSourceName')
+
+                if val is None:
+                    return None
+
+                sourceId = val
+
+            return self._controller.get_event(sourceId, event_id)
+
+        return None
+
+    @property
+    def app_name(self) -> str | None:
+        #Name of the current running app.
+        x = self._attr_app_name = self.GetSourceEvent("MetaData1") # NowPlayingSrceName
+        return self._attr_app_name
+
+
+    @property
+    def available(self) -> bool:
+        self._attr_available = self._controller.is_connected
+        return self._controller.is_connected
+
     @property
     def state(self) -> MediaPlayerState | None:
-        """Return the state of the device."""
-        if self._isOn:
-            return MediaPlayerState.ON
-        else:
-            return MediaPlayerState.OFF
+
+        if self._controller.is_connected:
+
+            self._isOn = False
+            self._attr_state = MediaPlayerState.OFF
+
+            if self._controller._mode == MODE_MRAD:
+                power = self._controller.get_event(self._mms_zone_id, 'PowerOn')
+            else:
+                power = 'True' # since MODE_STANDALONE zones (aka instances) are ALWAYS ON
+
+            if power is None:
+                return self._attr_state
+
+            elif power.find('T')==0:
+                self._isOn = True
+                self._attr_state = MediaPlayerState.ON
+
+                mediaControl = self.GetSourceEvent('MediaControl')
+
+                if mediaControl is not None:
+                    if mediaControl == 'Pause':
+                        self._attr_state = MediaPlayerState.PAUSED
+                    elif mediaControl == 'Stop':
+                        self._attr_state = MediaPlayerState.IDLE
+                    elif mediaControl == 'Play':
+                        self._attr_state = MediaPlayerState.PLAYING
+
+        return self._attr_state
+
 
     @property
     def available(self) -> bool:
@@ -149,18 +250,44 @@ class MmsZone(MediaPlayerEntity):
     @property
     def supported_features(self) -> MediaPlayerEntityFeature:
         """Flag media player features that are supported."""
-        return MediaPlayerEntityFeature.TURN_ON | MediaPlayerEntityFeature.TURN_OFF | MediaPlayerEntityFeature.GROUPING
-
-    """ @property
-    def source(self) -> str | None:
-        # Return the current input source.
-        return self._sourceName
+        return MediaPlayerEntityFeature.TURN_ON | MediaPlayerEntityFeature.TURN_OFF | MediaPlayerEntityFeature.GROUPING | MediaPlayerEntityFeature.SELECT_SOURCE
 
     @property
-    def source_list(self):
-        # List of available input sources.
-        return self._controller.clean_inputs
+    def source(self) -> str | None:
+        # Name of the current input source.
+        sourceName = None
 
+        if self._controller.is_connected:
+            sourceName = self._controller.get_event(self._mms_source_id, 'QualifiedSourceName')
+
+            if sourceName is not None and sourceName == "":
+                sourceName = None
+
+            if sourceName is not None:
+                sourceName = sourceName.split("@")[0]
+
+            if sourceName is None:
+                sourceName = self._controller.get_event(self._mms_source_id, 'SourceName')
+
+            if sourceName is not None and sourceName == "":
+                sourceName = None
+
+        return sourceName
+
+
+    @property
+    def source_list(self) -> list[str] | None:
+        # From ZoneGroups
+        # List of available input sources.
+        sourceList = None
+
+        if self._controller.is_connected:
+            sourceList = self._controller.get_event(self._mms_zone_id, 'SourceList')
+
+        return sourceList
+
+
+    """
     async def async_select_source(self, source):
         # Select input source.
         index = self._controller.video_inputs.index(source)+1
@@ -194,3 +321,29 @@ class MmsZone(MediaPlayerEntity):
 
         # Useful for making sensors
         return self._extra_attributes """
+
+    @property
+    def media_title(self) -> str | None:
+        # Title of current playing media.
+        self._attr_media_title = self.GetSourceEvent("MetaData4")
+        return self._attr_media_title
+
+
+    @property
+    def media_artist(self):
+        # Artist of current playing media, music track only.
+        self._attr_media_artist = self.GetSourceEvent("MetaData2")
+        return self._attr_media_artist
+
+    @property
+    def media_album_name(self):
+        # Album name of current playing media, music track only.
+        self._attr_media_artist = self.GetSourceEvent("MetaData3")
+        return self._attr_media_album_name
+
+    @property
+    def media_image_url(self) -> str | None:
+        # From ZoneGroups
+        # Image url of current playing media.
+        self._attr_media_image_url = self.GetSourceEvent("mArt")
+        return self._attr_media_image_url
